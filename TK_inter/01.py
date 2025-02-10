a86106f35ed5d60c0638494e5310dd058aaed727
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2025-02-10 17:32:32 +0200

    Create 01.py

---
+++ TK_inter/01.py
@@ -0,0 +1,7 @@
+import tkinter as tk #Bibliotekos įtraukimas į projektą
+
+window = tk.Tk() #Pagtrindinio lango sukūrimas
+label = tk.Label(text="Python rules!!!") #Antraštės sukūrimas
+label.pack() #Prijungiame antraštę prie pagrindinio lango
+
+window.mainloop() #Paleidžiame pagrindinį ciklą
